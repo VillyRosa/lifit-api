e57094c78098cbfbc2b75d51ab3be3b5e23ecf21
Villy Rosa <villyrosa69@gmail.com>
2025-10-16 22:58:00 -0300

    chore: initial commit

---
+++ src/test/java/com/lift/api/LiftApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lift.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
