8e1e91059c9fc39ea99e447de3fa3a180e38ed40
Villy Rosa <villyrosa69@gmail.com>
2025-10-21 00:40:24 -0300

    fix: only accept create one user_daily_data per day

--- src/main/java/com/lift/api/userDailyData/UserDailyDataRepository.java
+++ src/main/java/com/lift/api/userDailyData/UserDailyDataRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 public interface UserDailyDataRepository extends JpaRepository<UserDailyData, UUID> {
     Page<UserDailyData> findByUserId(UUID user_id, Pageable pageable);
+    boolean existsByUserIdAndCreatedAtBetween(UUID userId, OffsetDateTime startOfDay, OffsetDateTime endOfDay);
 }
--- src/main/java/com/lift/api/userDailyData/UserDailyDataService.java
+++ src/main/java/com/lift/api/userDailyData/UserDailyDataService.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.ZoneOffset;
 import java.util.UUID;
 
 @Service
@@ -31,10 +33,28 @@ public class UserDailyDataService {
                 .orElse(null);
     }
 
-
-    public UserDailyDataDTO create(NewUserDailyDataDTO userDailyData) {
+    public UserDailyDataDTO create(NewUserDailyDataDTO newUserDailyDataDTO) {
         var user = authService.getAuthenticatedUser();
-        return UserDailyDataMapper.map(userDailyDataRepository.save(UserDailyDataMapper.map(user, userDailyData)));
+
+        boolean exists = userDailyDataRepository.existsByUserIdAndCreatedAtBetween(
+                user.getId(),
+                LocalDate.now()
+                        .atStartOfDay()
+                        .atOffset(ZoneOffset.of("-03:00")),
+                LocalDate.now()
+                        .plusDays(1)
+                        .atStartOfDay()
+                        .atOffset(ZoneOffset.of("-03:00"))
+        );
+
+        if (exists) {
+            return null;
+        }
+
+        var data = new UserDailyData(user, newUserDailyDataDTO.weight(), newUserDailyDataDTO.height());
+        var saved = userDailyDataRepository.save(data);
+
+        return UserDailyDataMapper.map(saved);
     }
 
     public UserDailyDataDTO update(UUID id, NewUserDailyDataDTO newUserDailyDataDTO) {
